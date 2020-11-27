8ec3507d87276173eac3e50de02f41bb34e93c45
Toni García <74008049+ToniGar20@users.noreply.github.com>
2020-11-27 19:48:55 +0100

    Add files via upload

---
+++ bloc2-js/llmm-js-inicio/mini-ejercicios-js-inicio/ex2.js
@@ -0,0 +1,4 @@
+let a = "Maria";
+let b = "Jose";
+
+prompt("¿Cómo se llamaba la peñita del portal de Belén?":a "y "b);
---
+++ bloc2-js/llmm-js-inicio/mini-ejercicios-js-inicio/ex6.js
@@ -0,0 +1,6 @@
+var num1 = parseInt(prompt("Escribe un número"));
+var num2 = parseInt(prompt("Escribe otro número"));
+
+var result = num1+num2
+
+document.write("Oye colega, ¿sabías que la suma de tus dos cosas es "+ result + "?");
