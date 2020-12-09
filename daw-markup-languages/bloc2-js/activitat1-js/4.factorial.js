033d26eb31ee0d1e79dc6e217d617501e4a936a5
Toni García <garcia.t20@gmail.com>
2020-12-09 22:28:32 +0100

    working on exercice 4

---
+++ daw-markup-languages/bloc2-js/activitat1-js/4.factorial.js
@@ -0,0 +1,24 @@
+// let factorialQuestion = parseInt(prompt("¡Introduce un número para calcular su factorial!"));
+
+function calculateFactorial(number){
+    for (let i = number ; i > 0; i--){
+        let round = 1;
+        round *= i;
+        
+    }
+    return factorial;
+};
+
+document.write(calculateFactorial(5));
+
+function factorialIterativo(n) {
+    let s = 1;
+    while (n > 1) {
+        s *= n; // s = s*n
+        s +=
+        n--;
+    }
+    return s;
+}
+
+console.log(factorialIterativo(5));
