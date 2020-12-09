c65f9d01fb5e2a9f3fd58a5de7de370925beea1a
Toni García <garcia.t20@gmail.com>
2020-12-09 22:42:05 +0100

    exercice 4 with document-write

--- daw-markup-languages/bloc2-js/activitat1-js/4.factorial.js
+++ daw-markup-languages/bloc2-js/activitat1-js/4.factorial.js
@@ -1,9 +1,8 @@
-// let factorialQuestion = parseInt(prompt("¡Introduce un número para calcular su factorial!"));
-
+let number = parseInt(prompt("¡Introduce un número para calcular su factorial!"));
 function factorialCalculation(number) {
     for (i = 1; number > 1; number--) {
-      i*= number;
-    }
+        i*= number;
+        }
     return i;
-  };
-  console.log(factorialCalculation(5));
+};
+document.write("El factorial de " + number + " es " + factorialCalculation(number));
