9dad7430e69397bafde109208681f0b34485ed85
Toni <garcia.t20@gmail.com>
2020-12-11 18:11:46 +0100

    exercice 5 done

---
+++ daw-markup-languages/bloc2-js/activitat1-js/5.data.js
@@ -0,0 +1,25 @@
+function addZero(i) {
+    if (i < 10) {
+      i = "0" + i;
+    }
+    return i;
+  }
+function addOne(i) {
+    if (i < 12) {
+      i = 1 + i;
+    }
+    return i;
+  }
+  
+function calculateFullDate () {
+   let x = new Date();
+      let s = addZero(x.getSeconds());
+      let m = addZero(x.getMinutes());
+      let h = addZero(x.getHours());
+      let d = x.getDate();
+      let mn = addOne(x.getMonth());
+      let y = x.getFullYear();
+  return `Es el año ${y} y el mes ${mn}. El día es ${d} y son las ${h}:${m}:${s}`
+};
+
+document.write(calculateFullDate());
