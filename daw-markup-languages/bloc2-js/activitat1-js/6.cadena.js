090d97d8cfe72d6efeb043272638c01f13d1cfdf
Toni <garcia.t20@gmail.com>
2020-12-11 19:47:11 +0100

    finished exercice 6. I have made it without a function

--- daw-markup-languages/bloc2-js/activitat1-js/6.cadena.js
+++ daw-markup-languages/bloc2-js/activitat1-js/6.cadena.js
@@ -1,15 +1,7 @@
-/*
-6- Escriu un script que llegeixi una cadena de text i generi un array amb les paraules que conté. Després s'ha de mostrar la següent informació:
-
-Número de paraules que té l'array.
-Mostrar la primera paraula i la darrera paraula.
-Mostrar les paraules en ordre invers.
-*/
 
 let text = prompt("Introduce texto para sacar sus datos");
 
 let division = text.split("");
 document.write(`<ul><li>El texto tiene ${division.length} letras.</li>`);
-document.write(`<li>La primera letra es ${division[0]} y la última es... </li>`);
+document.write(`<li>La primera letra es ${division[0]} y la última es... ${division[division.length-1]}</li>`);
 document.write(`<li>${division.reverse()} es la cadena de texto al revés!</li></ul>`);
-
