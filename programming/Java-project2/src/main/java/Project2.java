5f595f8ab31bcce763c27f3ee0e77c35df7a5592
Toni <garcia.t20@gmail.com>
2020-12-21 20:32:16 +0100

    the application is build to say hello world

---
+++ programming/Java-project2/src/main/java/Project2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Project2 {
+    public static void main(String[] args){
+        System.out.println("hola mundo");
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+        System.out.println("You entered string "+s);
+        System.out.println("-END-");
+    }
+}
+
+//trozico de código que te dice hola mundo y que le metas algún texto para que te diga END!
