42cddc0d6328832652a5d4655e5ff5df9e292d31
Toni <garcia.t20@gmail.com>
2020-12-21 20:39:42 +0100

    application updated

--- programming/Java-project2/src/main/java/Project2.java
+++ programming/Java-project2/src/main/java/Project2.java
@@ -4,10 +4,11 @@ public class Project2 {
     public static void main(String[] args){
         System.out.println("hola mundo");
         Scanner in = new Scanner(System.in);
+        boolean isExit = false;
         String s = in.nextLine();
-        System.out.println("You entered string "+s);
+        System.out.println("You entered string " + s);
         System.out.println("-END-");
+
+        //trozico de código que te dice hola mundo y que le metas algún texto para que te diga END!
     }
 }
-
-//trozico de código que te dice hola mundo y que le metas algún texto para que te diga END!
